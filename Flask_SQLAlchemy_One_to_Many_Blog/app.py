63898158132ac7c39b24825e89a6bec47d528bd5
Mike Jackson <perigran_falcon@yahoo.com>
2025-05-25 20:04:33 -0600

    Fixed a comment orphan bug.

--- Flask_SQLAlchemy_One_to_Many_Blog/app.py
+++ Flask_SQLAlchemy_One_to_Many_Blog/app.py
@@ -21,12 +21,13 @@ class Post(db.Model):
     author = db.Column(db.String(100))
     title = db.Column(db.String(100))
     content = db.Column(db.Text)
-    comments = db.relationship('Comment', backref='post', lazy='dynamic')
+    comments = db.relationship(
+        "Comment", backref="post", lazy="dynamic", cascade="all, delete-orphan"
+    )
     created = db.Column(
         db.DateTime(timezone=True), server_default=db.func.current_timestamp()
     )
 
-    # Give object a string representation to recognize it for debugging.
     def __repr__(self):
         return f'<Post "{self.title}">'
 
@@ -35,12 +36,11 @@ class Comment(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     author = db.Column(db.String(100))
     content = db.Column(db.Text)
-    post_id = db.Column(db.Integer(), db.ForeignKey('post.id'))
+    post_id = db.Column(db.Integer(), db.ForeignKey("post.id"))
     created = db.Column(
         db.DateTime(timezone=True), server_default=db.func.current_timestamp()
     )
 
-    # Show the first 20 characters give the object a short string representation.
     def __repr__(self):
         return f'<Comment "{self.content[:20]}...">'
 
@@ -63,64 +63,68 @@ def index():
     return render_template("index.html", posts=posts)
 
 
-@app.route("/view/<int:post_id>", methods=('GET', 'POST'))
+@app.route("/view/<int:post_id>", methods=("GET", "POST"))
 def view(post_id):
     post = Post.query.get_or_404(post_id)
-    if request.method == 'GET':
+    if request.method == "GET":
         return render_template("view.html", post=post)
-    elif request.method == 'POST':
-        author = request.form['author']
-        content=request.form['content']
+    elif request.method == "POST":
+        author = request.form["author"]
+        content = request.form["content"]
         comment = Comment(author=author, content=content, post=post)
         db.session.add(comment)
         db.session.commit()
-        return redirect(url_for('view', post_id=post.id))
+        return redirect(url_for("view", post_id=post.id))
 
-@app.route('/create', methods = ['GET', 'POST'])
+
+@app.route("/create", methods=["GET", "POST"])
 def create():
-    if request.method == 'GET':
-        return render_template('create.html')
-    if request.method == 'POST':
-        author = request.form.get('author')
-        title = request.form.get('title')
-        content = request.form.get('content')
+    if request.method == "GET":
+        return render_template("create.html")
+    if request.method == "POST":
+        author = request.form.get("author")
+        title = request.form.get("title")
+        content = request.form.get("content")
         post = Post(author=author, title=title, content=content)
         db.session.add(post)
         db.session.commit()
-        return redirect(url_for('index'))
+        return redirect(url_for("index"))
 
-@app.route('/edit/<int:post_id>', methods = ['GET', 'POST'])
+
+@app.route("/edit/<int:post_id>", methods=["GET", "POST"])
 def edit(post_id):
     post = Post.query.get_or_404(post_id)
-    if request.method == 'GET':
-        return render_template('edit.html', post=post)
-    elif request.method == 'POST' :
-        post.title = request.form.get('title')
-        post.content = request.form.get('content')
+    if request.method == "GET":
+        return render_template("edit.html", post=post)
+    elif request.method == "POST":
+        post.title = request.form.get("title")
+        post.content = request.form.get("content")
         db.session.commit()
         return redirect(url_for("index"))
 
-@app.route('/comments')
+
+@app.route("/comments")
 def comments():
     comments = Comment.query.all()
-    return render_template('comments.html', comments=comments)
+    return render_template("comments.html", comments=comments)
 
-@app.route("/delete/<int:post_id>")
-def delete(post_id):
-    post = Post.query.get(post_id)
-    comment = Comment.query.get_or_404(post_id)
-    db.session.delete(comment)
+
+@app.route("/delete_post/<int:post_id>")
+def delete_post(post_id):
+    post = Post.query.get_or_404(post_id)
     db.session.delete(post)
     db.session.commit()
     return redirect(url_for("index"))
 
-@app.post('/delete_comment/<int:comment_id>')
+
+@app.post("/delete_comment/<int:comment_id>")
 def delete_comment(comment_id):
     comment = Comment.query.get_or_404(comment_id)
     post_id = comment.post.id
     db.session.delete(comment)
     db.session.commit()
-    return redirect(url_for('view', post_id=post_id))
+    return redirect(url_for("view", post_id=post_id))
+
 
 @app.route("/about")
 def about():
